727020e00bf75a6ebe317e7249ad256ac5ff9f47
keizien <ton-email-github@example.com>
2025-12-29 15:14:15 +0100

    Création de la classe Block, à voir si on fait d'autres changements

---
+++ Tetris.App/Block.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace Tetris.Logic
+{
+    public class Block
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        public Block(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public void Move(int dx, int dy)
+        {
+            X += dx;
+            Y += dy;
+        }
+    }
+}
--- Tetris.App/Game.cs
+++ Tetris.App/Game.cs
@@ -2,7 +2,6 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
-using Tetris.Logic;
 
 namespace Tetris.App
 {
